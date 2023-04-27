76642565990ca13bec9c334f7b4578f769cc4009
Isabel N. Gomes <isabel2012natalia@gmail.com>
2023-04-26 23:26:05 -0300

    Formulário do exercício 1 pronto.

--- ListaExerc1_1.php
+++ ListaExerc1_1.php
@@ -4,15 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cálculo de Equação do 2° Grau:</title>
 </head>
 <body>
     <form action = "ListaExerc1_1.php" method = "post">
 
     <table>
         <tr>
-            <th>Cálculo da Equação do 2° Grau: </th>
+            <th colspan = "2">Cálculo da Equação do 2° Grau: </th>
         </tr>
-        
+        <tr>
+            <td><label for = "lb1">a: </label></td>
+            <td><input type = "text" name = "a"></td>
+        </tr>
+        <tr>
+            <td><label for = "lb2">b: </label></td>
+            <td><input type = "text" name = "b"></td>
+        </tr>
+        <tr>
+            <td><label for = "lb3">c: </label></td>
+            <td><input type = "text" name = "c"></td>
+        </tr>
+        <tr>
+            <th colspan = "2">MENSAGENS: </th>
+        </tr>
 </body>
 </html>
