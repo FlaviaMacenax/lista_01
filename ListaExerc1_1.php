2fecfca68224f353eea789bf5c14114988058ed2
Isabel N. Gomes <isabel2012natalia@gmail.com>
2023-05-02 21:07:03 -0300

    AAAAAA

--- ListaExerc1_1.php
+++ ListaExerc1_1.php
@@ -9,29 +9,33 @@ $botao = "";
 if(isset($_POST["calcular"])) {
     $botao = $_POST["calcular"];
 
-    if(isset($_POST["a"])) {
-        $a = $_POST["a"];
+if ($a == NULL || $b == NULL || $c == NULL) {
+     $msg = "É preciso preencher os campos para calcular as raízes.";
+}
+else {
+    if ($a == 0) {
+        $msg = "Insira um número diferente de 0 (zero) para A."
     }
-    if(isset($_POST["b"])) {
-        $b = $_POST["b"];
-    }
-    if(isset($_POST["c"])) {
-        $c = $_POST["c"];
-    }
-
-    if ($a != 0) {
-        $delta = $b * $b - 4 * $a * $c;
-
+    
+    else {
+        $delta = ($b * $b) - (4 * $a * $c);
         if ($delta > 0) {
-            $raiz1 = (-$b + sqrt($delta)) / (2 * $a);
-            $raiz2 = (-$b - sqrt($delta)) / (2 * $a);
-        } elseif ($delta == 0) 
-            $raiz1 = $raiz2 = -($b) / (2 * $a); 
-        //Colocar as chaves caso não dê certo
+            $x1 = ((-1 * b) + sqrt($delta)) / 2 * $a;
+            $x2 = ((-1 * b) - sqrt($delta)) / 2 * $a;
+            $msg = "As raízes da equação, respectivamente, são " . $x1 . " e " . $x2 . ".";
+        }
+
+        else if ($delta == 0) {
+            $x = (-1 * b) / 2 * $a;
+            $msg = "A raíz da equação é " . $x . ".";
+        }
+
         else {
-            $real = -$b / (2 * a)
+            $msg = "Não existem raízes reais para esta equação."
         }
     }
+}
+
 }
 
 ?>
@@ -49,7 +53,7 @@ if(isset($_POST["calcular"])) {
 
     <table>
         <tr>
-            <th colspan = "2">Cálculo da Equação do 2° Grau: </th>
+            <th colspan = "2">Cálculo da Equação do 2° Grau: ax² + bx + c = 0</th>
         </tr>
         <tr>
             <td><label for = "lb1">a: </label></td>
@@ -67,7 +71,7 @@ if(isset($_POST["calcular"])) {
             <td colspan = "2"><input class = "button" type = "submit" name = "calcular"></td>
         </tr>
         <tr>
-            <th colspan = "2">MENSAGENS: </th>
+            <th colspan = "2">MENSAGENS: <?php echo $msg?></th>
         </tr>
 </body>
 </html>
